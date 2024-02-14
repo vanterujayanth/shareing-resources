c4dd9c0276f6c6b51bbe1eb0f3ed5f7552d3f2f0
billuramya <129533827+billuramya@users.noreply.github.com>
2024-02-14 12:02:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+/*
+ public class DoWhileExample
+{
+    public static void Main(string[] args)
+    {
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 10);
+
+    }
+}
+*/
+
+/*public class WhileExample
+{
+    public static void Main(string[] args)
+    {
+        int i = 1;
+        while (i<=10) {
+            Console.WriteLine(i);
+            i++;
+        }
+
+    }
+}
+*/
+
+/*
+  class Simle
+{
+     static void Main (string[] args)
+    {
+        System.Console.WriteLine("hello ,welcome");
+        string name = "Goutham";
+        int age = 30;
+        double d = 3.12;
+        var n = 30.9;
+        float val = 14567;
+
+    }
+}
+*/
+
+/*
+public class ForloopExample
+{
+    static void Main (string[] args)
+    {
+        int num = 10;
+        for (int i=0;i<=num;i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+*/
+
+/*
+ public class NestedtForLoopExample
+{
+  public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first value");
+        int Value1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second value");
+        int Value2 = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= Value1; i++) {
+            for (int j = 1; j <= Value2; j++)
+            {
+                Console.WriteLine(i + " " + j);
+            }
+        }
+    }
+
+}
+*/
+
+/*
+public class doWhileLoopExample
+{
+    public static void Main(string[] args)
+    {
+        int num = 5;
+        int userInput = Convert.ToInt32(Console.ReadLine());
+        do
+        {
+
+            Console.WriteLine("num value is less than userInput");
+            break;
+           
+        } while (num <= userInput);
+            
+        
+    }
+
+}
+*/
+
+/*
+public class ConditionalOperators
+{
+    public static void Main(string[] args)
+    {
+        int num = 13;
+        if (num %2 == 0)
+        {
+            Console.WriteLine("It Is even Number");
+        }
+        else
+        {
+            Console.WriteLine("It Is A Odd Number");
+        }
+        
+    }
+}
+*/
+
+/*
+public class if_else_if_Conditions
+{
+    public static void Main(string[] args)
+    {
+        int num = 15;
+        Console.WriteLine("enter user input");
+        int userInput = Convert.ToInt32(Console.ReadLine());    
+        if(num > userInput)
+        {
+            Console.WriteLine("number is greater than  userInput");
+        } else if (num < userInput) {
+            Console.WriteLine("number is less than  userInput");
+        }
+        else {
+            Console.WriteLine("number and userInput both are same");
+                }
+    }
+}
+*/
+
+/*
+public class switchCaseExample
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter the weeks name");
+        string month = Convert.ToString(Console.ReadLine());
+        switch (month)
+        {
+            case ("monday"):
+                Console.WriteLine("TODAY MONDAY");
+                break;
+            case ("tuesday"):
+                Console.WriteLine("TODAY WAS TUEADAY");
+                break;
+            case ("wednesday"):
+                Console.WriteLine("TODAY WAS WEDNESDAY");
+                break;
+            case ("thursday"):
+                Console.WriteLine("TODAY WAS THURSDAY");
+                break;
+            case ("friday"):
+                Console.WriteLine("TODAY WAS FRIDAY");
+                break;
+            case ("satueday"):
+                Console.WriteLine("TODAY WAS SATURDAY");
+                break;
+
+            default:
+                Console.WriteLine("is not a week name");
+                break; 
+           
+
+        }
+    }
+} 
+*/ 
